2a3c2931b20eeb153151185d458379900dfd419f
UniqueZhang <95599299+UniqueZhang@users.noreply.github.com>
2022-05-17 11:42:15 +0800

    Add files via upload

---
+++ main_exp1.m
@@ -0,0 +1,39 @@
+clc
+close all
+clear all
+%% Problem Setup 
+
+[machine1,parts1,machine_data1,parts_data1]=data_input('C:\Users\ASUS\Desktop\BL\2\experiment1\Test instances of Che et al., 2021\ht100_3.txt');
+%生成多组染色体并解码
+ProbData.n = 2*size(parts_data1,2);
+ProbData.m = 5;
+
+alldata.machine = machine1;
+alldata.parts = parts1;
+alldata.machinedata = machine_data1;
+alldata.partsdata = parts_data1;
+
+
+%% Algorithm Setup
+
+% AlgOptions: Options of the Algorithm
+%  - CreationFcn: [function handle] The function creating the initial pop
+
+% ==== Outputs ====
+% F1: Non-dominated Solutions
+% pop: Final Population 
+% t_run: running time
+
+AlgOptions.Generations = 10;
+AlgOptions.PopulationSize = 50;
+AlgOptions.CreationFcn = @Creation;%初始化
+
+
+ObjFun = @CalObjValues;%目标函数的计算
+[ F1_exp1, pop_exp1 , t_run] = ALGRANDOM(ObjFun, AlgOptions, ProbData,alldata)
+%% 数据的存储
+save('data_exp1\experiment1T1.mat','F1_exp1','pop_exp1');
+
+
+
+
